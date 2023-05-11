762e1c15ce8e24ffb2a1783e27a1b1649cdca2ba
Raijin1302 <raijinnguyen1302@gmail.com>
2023-05-11 23:59:23 +0700

    :red_circle: setting connectDb

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import compression from "compression"
 import cookieParser from "cookie-parser"
 import http from "http"
 import cors from "cors"
-
+import mongoose from "mongoose"
 const app = express()
 
 app.use(
@@ -13,21 +13,24 @@ app.use(
     credentials: true,
   })
 )
-
 app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
 
 const server = http.createServer(app)
 const port = process.env.PORT || 5000
+const connectDB = require("./db/connect")
+mongoose.Promise = Promise
 
 const start = async () => {
   try {
+    await connectDB(process.env.DATABASE_URL)
+    //mongoose.connect(process.env.DATABASE_URL)
     server.listen(port, () => {
       console.log(`Server is running on ${port}`)
     })
   } catch (error) {
-    console.log(error)
+    mongoose.connection.on("error", (error: Error) => console.log(error))
   }
 }
 start()
