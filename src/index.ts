bf7b0ed3e2c24a373ef59e7eee69c1eca1d3cae3
Raijin1302 <raijinnguyen1302@gmail.com>
2023-05-13 23:42:25 +0700

    :red_circle: add routes auth

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import compression from "compression"
 import cookieParser from "cookie-parser"
 import http from "http"
 import cors from "cors"
+
+import router from "./router"
 import mongoose from "mongoose"
 const app = express()
 
@@ -19,18 +21,24 @@ app.use(bodyParser.json())
 
 const server = http.createServer(app)
 const port = process.env.PORT || 5000
-const connectDB = require("./db/connect")
-mongoose.Promise = Promise
+// const connectDB = require("./db/connect")
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
 
-const start = async () => {
-  try {
-    await connectDB(process.env.DATABASE_URL)
-    //mongoose.connect(process.env.DATABASE_URL)
-    server.listen(port, () => {
-      console.log(`Server is running on ${port}`)
-    })
-  } catch (error) {
-    mongoose.connection.on("error", (error: Error) => console.log(error))
-  }
-}
-start()
+mongoose.Promise = Promise
+mongoose.connect(process.env.DATABASE_UR)
+mongoose.connection.on("error", (error: Error) => console.log(error))
+app.use("/", router())
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.DATABASE_URL)
+//     //mongoose.connect(process.env.DATABASE_URL)
+//     server.listen(port, () => {
+//       console.log(`Server is running on ${port}`)
+//     })
+//   } catch (error) {
+//     mongoose.connection.on("error", (error: Error) => console.log(error))
+//   }
+// }
+// start()
